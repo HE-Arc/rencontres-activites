2454d28c8aed9622e3cc47498fdb6ad9f57501bc
Diogo-Ferreira <d.venancio.ferreira@gmail.com>
2017-02-22 14:21:33 +0100

    class based view for activities

--- meethub/app/views.py
+++ meethub/app/views.py
@@ -1,8 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
 
 
 def index(request):
     # just to check if template works
     return render(request,'activity/create.html')
+
+
+class Activity(View):
+    def get(self, request):
+        return HttpResponse("GET")
+
+    def post(self, request):
+        return HttpResponse("POST")
