f1e764be1279d852da2e43d2f0b39f43e860c043
Diogo-Ferreira <d.venancio.ferreira@gmail.com>
2017-02-27 12:51:47 +0100

    update and create view

--- meethub/app/views.py
+++ meethub/app/views.py
@@ -3,16 +3,32 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
+from django.views.generic import CreateView
+from django.views.generic import FormView
+from django.views.generic import UpdateView
+
+from .forms import ActivityForm
+from .models import Activity as ActivityModel
 
 
 def index(request):
     # just to check if template works
-    return render(request,'activity/create.html')
+    return render(request, 'activity/create.html')
 
 
 class Activity(View):
     def get(self, request):
         return HttpResponse("GET")
 
-    def post(self, request):
-        return HttpResponse("POST")
+
+class ActivityFormViewCreate(CreateView):
+    template_name = 'activity/create.html'
+    form_class = ActivityForm
+    success_url = '/'
+
+
+class ActivityFormViewUpdate(UpdateView):
+    template_name = 'activity/create.html'
+    form_class = ActivityForm
+    success_url = '/'
+    model = ActivityModel
