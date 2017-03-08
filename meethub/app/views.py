a48c196bd66e4654526cd019f50fee980b391c4b
Daniel Rodrigues Lourenço <daniel.24.rodrigues@gmail.com>
2017-03-08 19:31:22 +0100

    Make friendly template contexts for Activity details page

--- meethub/app/views.py
+++ meethub/app/views.py
@@ -26,3 +26,4 @@ class ActivityFormViewUpdate(UpdateView):
 class ActivityDetailView(generic.DetailView):
     model = Activity
     template_name = 'activity/index.html'
+    context_object_name = 'activity'
