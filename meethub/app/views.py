4f8d32be2c5003ae87b9ffab60c53ab188f877c9
BryanMums <bryan.muhmenthaler@gmail.com>
2017-03-22 12:12:00 +0100

    Authentification de base. A finir (Passage de Windows à Linux :tada:)

--- meethub/app/views.py
+++ meethub/app/views.py
@@ -10,7 +10,24 @@ from .models import Activity
 from .forms import ActivityForm
 
 def index(request):
-    return render(request,'pages/dashboard.html')
+    return render(request, 'pages/index.html')
+
+def dashboard(request):
+    # if request.user.is_authenticated :
+    #    render(request, 'pages/index.html')
+    context = {}
+    # Get the informations of the logged user
+
+    # Get the three latest activities done by the user
+
+    # Get the three next activities of the user
+
+    # Get all the activities that are opened next to the user
+
+
+    activities = Activity.objects.all();
+    context['activities'] =  activities;
+    return render(request,'pages/dashboard.html', context)
 
 class ActivityFormViewCreate(CreateView):
     template_name = 'activity/create.html'
--- meethub/meethub/settings.py
+++ meethub/meethub/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'geoposition',
+    'bootstrap4',
     'django.contrib.sites',
     'disqus',
 ]
@@ -123,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SITE_ID = 1
+
+ACCOUNT_ACTIVATION_DAYS = 7
+
+LOGIN_REDIRECT_URL='dashboard'
